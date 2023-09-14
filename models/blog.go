cbf16fa98b3891f29449f122d28cba164e64c03e
swain778 <swain.shub@gmail.com>
2023-09-14 20:29:05 +0530

    + Post data model

---
+++ models/blog.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Post struct {
+	ID        uint           `json:"id" gorm:"type:uuid;primary_key"`
+	Title     string         `json:"title"`
+	Content   string         `json:"content" gorm:"not null"`
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `json:"deletedAt" sql:"index:deleted_at"`
+}
